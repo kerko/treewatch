f836b44c33ef67a5e33a9ffda6856f9d12bce2ec
Mazorius <mail@ron.gebauers.org>
2015-10-13 11:14:58 +0200

    #326 time:0.2

--- Pages/Map/MapMenuContentPage.xaml.cs
+++ Pages/Map/MapMenuContentPage.xaml.cs
@@ -12,6 +12,47 @@ namespace TreeWatch
 			InitializeComponent ();
 
 			Title = "Settings";
+
+			int rowCount = 5;
+			RowDefinitionCollection rows = new RowDefinitionCollection();
+			for (int i = 0; i < rowCount; i++) 
+			{
+				rows.Add(new RowDefinition { Height = GridLength.Auto });
+			}
+
+
+			Grid grid = new Grid
+			{
+				VerticalOptions = LayoutOptions.FillAndExpand,
+				RowDefinitions = rows,
+				ColumnDefinitions = 
+				{
+					new ColumnDefinition { Width = new GridLength (1, GridUnitType.Star)},
+					new ColumnDefinition { Width = new GridLength (1, GridUnitType.Star)}}
+				};
+			for(int i = 0; i < rowCount; i++)
+			{
+				grid.Children.Add(new Label
+					{
+						Text = "Layout " + i,
+						FontSize = 25,
+						HorizontalOptions = LayoutOptions.StartAndExpand
+					}, 0, i);
+
+				Switch switcher = new Switch
+				{
+					HorizontalOptions = LayoutOptions.End,
+					VerticalOptions = LayoutOptions.CenterAndExpand
+				};
+				switcher.Toggled += switcher_Toggled;
+				grid.Children.Add (switcher, 1, i);
+			}
+			Content = grid;
+		}
+
+		void switcher_Toggled(object sender, ToggledEventArgs e)
+		{
+			//TODO
 		}
 	}
 }
