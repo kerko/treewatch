dc06c57aaf3e9e15d2c848065057efef9702d6b0
Mazorius <mail@ron.gebauers.org>
2015-10-15 14:48:06 +0200

    #326 comment:update file structure and read into mvvm

---
+++ View/Note/NoteContentPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace TreeWatch
+{
+	public partial class NoteContentPage : ContentPage
+	{
+		public NoteContentPage ()
+		{
+			InitializeComponent ();
+
+			//site configurations
+			Title = "Note";
+			if (Device.OS == TargetPlatform.iOS) {
+				Icon = "NoteTabBarIcon.png";
+			}
+
+			//filler
+			BackgroundColor = Color.Aqua;
+			siteLabel.Text = Title;
+		}
+	}
+}
+
---
+++ View/ToDo/ToDoContentPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace TreeWatch
+{
+	public partial class ToDoContentPage : ContentPage
+	{
+		public ToDoContentPage ()
+		{
+			InitializeComponent ();
+
+			//site configurations
+			Title = "ToDo";
+			if (Device.OS == TargetPlatform.iOS) {
+				Icon = "ToDoTabBarIcon.png";
+			}
+
+			//filler
+			BackgroundColor = Color.Aqua;
+			siteLabel.Text = Title;
+		}
+	}
+}
+
